58177518c40bcb8f86c23794b8c3b18c62a7582b
Rob Prado <robpradodev@gmail.com>
2020-08-29 13:21:42 -0300

    Starting with React native core components

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{padding: 30}}>
+      <View>
+        <TextInput placeholder="Course Goal" style={{borderBottomColor:'black', borderBottomWidth: 1}}/>
+        <Button title="ADD" />
+      </View>
+      <View>
+
+      </View>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
